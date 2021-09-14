5c5916f30d72bf7772fe469c4df1bbcc448192ce
Suhailol <suhailmoidin96@gmail.com>
2021-09-14 21:39:10 +0530

    Creating Transfer, TransferFrom and approve function

--- Token.sol
+++ Token.sol
@@ -18,5 +18,28 @@ contract Token {
     function balanceOf(address owner) public returns(uint) {
         return balances[owner];
     }
+    function transfer(address to, uint value) public returns(bool) {
+        require(balanceOf(msg.sender) >= value, 'balance too low');
+        balances[to] += value;
+        balances[msg.sender] -= value;
+       emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint value) public returns(bool) {
+        require(balanceOf(from) >= value, 'balance too low');
+        require(allowance[from][msg.sender] >= value, 'allowance too low');
+        balances[to] += value;
+        balances[from] -= value;
+        emit Transfer(from, to, value);
+        return true;   
+    }
+    
+    function approve(address spender, uint value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;   
+    }
+}
     
     
