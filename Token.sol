2df7b6cef203cec72759a80a4eb17c3760e44e38
hasthik <hasthikdjathan644@gmail.com>
2021-09-14 09:33:43 +0530

    added balance fuction

---
+++ Token.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.2;
+
+contract Token {
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowance;
+    uint public totalSupply = 10000008 * 10 ** 18;
+    string public name = "BION";
+    string public symbol = "BIN";
+    uint public decimals = 18;
+    
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+    
+    function balanceOf(address owner) public returns(uint) {
+        return balances[owner];
+    }
+    
+    
